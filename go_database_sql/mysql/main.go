18ed977719e47688f6f67b8a9f608fb47fd2bae7
linyu203 <linyu203@gmail.com>
2020-05-21 22:45:47 -0400

    BondsList

--- go_database_sql/mysql/main.go
+++ go_database_sql/mysql/main.go
@@ -7,6 +7,7 @@ import (
     "log"
     "net/http"
     "os"
+    //"github.com/gorilla/mux"
 )
 
 var (
@@ -20,7 +21,7 @@ func initTemplates() {
     var err error = nil
     indexTmpl, err = template.ParseFiles("templates/index.html")
     if err == nil {
-        //bondListTmpl, err = template.ParseFiles("templates/BondList.html")
+        bondListTmpl, err = template.ParseFiles("templates/BondList.html")
     }
     if err == nil {
         //newFundTmpl, err = template.ParseFiles("templates/NewFund.html")
@@ -32,15 +33,38 @@ func initTemplates() {
 
 func regestHandler(){
     http.HandleFunc("/", getAllFundHandler)
+    http.HandleFunc("/bonds/{fundName:[0-9a-zA-Z_\\-]+}", bondsHandler)
+
+
+
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-
     log.Printf("Listening on port %s", port)
     if err := http.ListenAndServe(":"+port, nil); err != nil {
         log.Fatal(err)
     }
+}
+
+func bondsHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("r: %#v", &r)
+    /*switch r.Method {
+    case "GET":
+        bonds, err := GetAllfunds()
+        if err != nil {
+            log.Printf("Get all funds: %v", err)
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+        err = indexTmpl.Execute(w, funds)
+        fmt.Printf("funds: %#v\n", funds)
+        if err != nil {
+            log.Printf("Transefor to html error: %v", err)
+        }
+    default:
+        http.Error(w, fmt.Sprintf("HTTP Method %s Not Allowed", r.Method), http.StatusMethodNotAllowed)
+    }*/
 }
 func getAllFundHandler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
@@ -51,15 +75,18 @@ func getAllFundHandler(w http.ResponseWriter, r *http.Request) {
             http.Error(w, "Internal Server Error", http.StatusInternalServerError)
             return
         }
-	err = indexTmpl.Execute(w, funds)
-	fmt.Printf("funds: %#v\n", funds)
+        err = indexTmpl.Execute(w, funds)
+        fmt.Printf("funds: %#v\n", funds)
         if err != nil {
-	    log.Printf("Transefor to html error: %v", err)
+            log.Printf("Transefor to html error: %v", err)
         }
     default:
         http.Error(w, fmt.Sprintf("HTTP Method %s Not Allowed", r.Method), http.StatusMethodNotAllowed)
     }
 }
+
+
+
 func main(){
     initTemplates()
     InitTable()
