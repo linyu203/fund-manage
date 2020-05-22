ac24a92f9951dc5724bb91f3739442e640012ac3
linyu203 <linyu203@gmail.com>
2020-05-22 00:04:34 -0400

    Revert "change register"
    This reverts commit 02b491af6d4da1016999c178d5d8652ba7cbe8c5.

--- go_database_sql/mysql/main.go
+++ go_database_sql/mysql/main.go
@@ -6,9 +6,8 @@ import (
     "html/template"
     "log"
     "net/http"
-    "io"
-	"os"
-    "github.com/gorilla/mux"
+    "os"
+    //"github.com/gorilla/mux"
 )
 
 var (
@@ -16,13 +15,11 @@ var (
     indexTmpl    *template.Template//= "index.html"
     bondListTmpl *template.Template//= "BondList.html"
     newFundTmpl  *template.Template//= "NewFund.html"
-    logWriter    io.Writer
 )
 
 func initTemplates() {
     var err error = nil
     indexTmpl, err = template.ParseFiles("templates/index.html")
-    logWriter = os.Stderr
     if err == nil {
         bondListTmpl, err = template.ParseFiles("templates/BondList.html")
     }
@@ -35,10 +32,10 @@ func initTemplates() {
 }
 
 func regestHandler(){
-    r := mux.NewRouter()
-    r.Methods("GET").Path("/").Handler(appHandler(getAllFundHandler))
-    r.Methods("GET").Path("/books/{id:[0-9a-zA-Z_\\-]+}").Handler(appHandler(bondsHandler))
-    http.Handle("/", handlers.CombinedLoggingHandler(logWriter, r))
+    http.HandleFunc("/", getAllFundHandler)
+    http.HandleFunc("/bonds/{fundName:[0-9a-zA-Z_\\-]+}", bondsHandler)
+
+
 
     port := os.Getenv("PORT")
     if port == "" {
@@ -51,7 +48,7 @@ func regestHandler(){
 }
 
 func bondsHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("bondsHandler called %v", r.Method)
+    fmt.Printf("r: %#v", &r)
     /*switch r.Method {
     case "GET":
         bonds, err := GetAllfunds()
@@ -70,17 +67,21 @@ func bondsHandler(w http.ResponseWriter, r *http.Request) {
     }*/
 }
 func getAllFundHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("getAllFundHandler called %v", r.Method)
-    funds, err := GetAllfunds()
-    if err != nil {
-        log.Printf("Get all funds: %v", err)
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        return
-    }
-    err = indexTmpl.Execute(w, funds)
-    fmt.Printf("funds: %#v\n", funds)
-    if err != nil {
-        log.Printf("Transefor to html error: %v", err)
+    switch r.Method {
+    case "GET":
+        funds, err := GetAllfunds()
+        if err != nil {
+            log.Printf("Get all funds: %v", err)
+            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+            return
+        }
+        err = indexTmpl.Execute(w, funds)
+        fmt.Printf("funds: %#v\n", funds)
+        if err != nil {
+            log.Printf("Transefor to html error: %v", err)
+        }
+    default:
+        http.Error(w, fmt.Sprintf("HTTP Method %s Not Allowed", r.Method), http.StatusMethodNotAllowed)
     }
 }
 
